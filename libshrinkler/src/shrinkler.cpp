1225b6a916ca8e4caa46ccb59c5bab77c2eb38d4
tom42 <tom42@users.noreply.github.com>
2021-12-16 23:28:02 +0100

    Partially deleted progress display code using printf.

--- libshrinkler/src/shrinkler.cpp
+++ libshrinkler/src/shrinkler.cpp
@@ -62,24 +62,11 @@ static vector<uint32_t> compress(vector<unsigned char>& data, PackParams& params
     vector<uint32_t> pack_buffer;
     RangeCoder range_coder(LZEncoder::NUM_CONTEXTS + NUM_RELOC_CONTEXTS, pack_buffer);
 
-    // Print compression status header
-    // TODO: get rid of this.
-    const char* ordinals[] = { "st", "nd", "rd", "th" };
-    printf("Original");
-    for (int p = 1; p <= params.iterations; p++) {
-        printf("  After %d%s pass", p, ordinals[min(p, 4) - 1]);
-    }
-    printf("\n");
-
     // Crunch the data
     range_coder.reset();
     // TODO: remember why we duplicated packData too. Or Fix it. What IS it printing here?
     packData(&data[0], numeric_cast<int>(data.size()), 0, &params, &range_coder, &edge_factory, show_progress);
     range_coder.finish();
-
-    // TODO: get rid of this
-    printf("\n\n");
-    fflush(stdout);
 
     return pack_buffer;
 }
