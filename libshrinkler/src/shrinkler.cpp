826569df59837a4e9a0c96c7795d3ea89dabbb4b
tom42 <tom42@users.noreply.github.com>
2021-12-17 19:50:04 +0100

    Replaced numeric_cast in verify by cast to signed type.

--- libshrinkler/src/shrinkler.cpp
+++ libshrinkler/src/shrinkler.cpp
@@ -26,6 +26,7 @@
 #include <boost/numeric/conversion/cast.hpp>
 #include <cstdint>
 #include <iostream>
+#include <type_traits>
 #include "fmt/core.h"
 #include "libshrinkler/libshrinkler.hpp"
 
@@ -70,8 +71,14 @@ static vector<uint32_t> compress(vector<unsigned char>& data, PackParams& params
     return pack_buffer;
 }
 
+template <typename T>
+std::make_signed_t<T> signed_cast(T value)
+{
+    return value;
+}
+
 // Corresponds to DataFile::verify in Shrinkler.
-int verify(vector<unsigned char>& data, vector<uint32_t>& pack_buffer, PackParams& params)
+auto verify(vector<unsigned char>& data, vector<uint32_t>& pack_buffer, PackParams& params)
 {
     CONSOLE << "Verifying..." << endl;
 
@@ -96,11 +103,7 @@ int verify(vector<unsigned char>& data, vector<uint32_t>& pack_buffer, PackParam
     // TODO: is the number printed here correct? (maybe test with lost marbles)
     //       And it seems to be possible that this becomes negative for very small input?
     //       => Well we can somehow return this as a result and have unit tests check it, no?
-    // TODO: we DO not want a numeric_cast here. What we do want is to return a suitable
-    //       signed integer type, e.g. ssize_t or maybe ptrdiff_t. We can check this if
-    //       we disable the parity context. With the current code this will throw, because
-    //       the margin ends up -7, but due to the numeric_cast, that conversion from unsigned to signed will throw.
-    return numeric_cast<int>(verifier.front_overlap_margin + pack_buffer.size() * 4 - data.size());
+    return signed_cast(verifier.front_overlap_margin + pack_buffer.size() * 4 - data.size());
 }
 
 static vector<unsigned char> to_little_endian(const vector<uint32_t>& pack_buffer)
