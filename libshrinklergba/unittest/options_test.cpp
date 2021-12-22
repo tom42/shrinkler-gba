3b4bcd6bb6b0ce76db70a9063d629829b44ebf87
tom42 <tom42@users.noreply.github.com>
2021-12-22 22:04:00 +0100

    Set up test for options class.

---
+++ libshrinklergba/unittest/options_test.cpp
@@ -0,0 +1,43 @@
+// MIT License
+//
+// shrinkler-gba: Port of the Shrinkler Amiga executable cruncher for the GBA
+// Copyright (c) 2021 Thomas Mathys
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <boost/test/unit_test.hpp>
+#include "libshrinklergba/options.hpp"
+
+namespace libshrinklergba_unittest
+{
+
+using libshrinklergba::options;
+
+BOOST_AUTO_TEST_SUITE(options_test)
+
+    BOOST_AUTO_TEST_CASE(constructor)
+    {
+        // TODO: check stuff here, e.g. input file name, output file name, verbose flag
+        //options testee;
+        BOOST_FAIL("YIKES");
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}
