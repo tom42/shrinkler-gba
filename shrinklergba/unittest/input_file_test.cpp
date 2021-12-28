9e94941a9a04d2920665b1d45c67b1e778e842f0
tom42 <tom42@users.noreply.github.com>
2021-12-29 00:22:24 +0100

    Include string predicates only.

--- shrinklergba/unittest/input_file_test.cpp
+++ shrinklergba/unittest/input_file_test.cpp
@@ -21,7 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/predicate.hpp>
 #include <boost/test/unit_test.hpp>
 #include <filesystem>
 #include <stdexcept>
