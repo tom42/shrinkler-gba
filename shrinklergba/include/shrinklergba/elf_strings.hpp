0bfd4003d8801436723ce01fc445d782ecfa364a
tom42 <tom42@users.noreply.github.com>
2022-01-10 17:53:00 +0100

    Created elf_strings module with to_hex replacement

---
+++ shrinklergba/include/shrinklergba/elf_strings.hpp
@@ -0,0 +1,43 @@
+// MIT License
+//
+// shrinkler-gba: Port of the Shrinkler Amiga executable cruncher for the GBA
+// Copyright (c) 2021 Thomas Mathys
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef SHRINKLERGBA_ELF_STRINGS_HPP
+#define SHRINKLERGBA_ELF_STRINGS_HPP
+
+#include <string>
+#include "fmt/core.h"
+#include "shrinklergba/elfio_wrapper.hpp"
+
+namespace shrinklergba::elf_strings
+{
+
+template <typename TValue, typename NDigits>
+std::string to_hex(TValue value, NDigits ndigits)
+{
+    const auto prefix_width = 2;
+    return format("{:#0{}x}", value, ndigits + prefix_width);
+}
+
+}
+
+#endif
