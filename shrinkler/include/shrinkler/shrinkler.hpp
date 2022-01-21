68c4c00d20b2d81b8da6ecfe2f669f3976d5173e
tom42 <tom42@users.noreply.github.com>
2022-01-21 11:31:18 +0100

    shrinkler_parameters: added preset(), so that preset can be applied after construction.

--- shrinkler/include/shrinkler/shrinkler.hpp
+++ shrinkler/include/shrinkler/shrinkler.hpp
@@ -39,22 +39,27 @@ class shrinkler_parameters
 {
 public:
     explicit shrinkler_parameters(int preset = 2)
-        : iterations(1 * preset),
-        length_margin(1 * preset),
-        same_length(10 * preset),
-        effort(100 * preset),
-        skip_length(1000 * preset),
-        references(100000)
-    {}
+    {
+        this->preset(preset);
+    }
+
+    void preset(int preset)
+    {
+        iterations = 1 * preset;
+        length_margin = 1 * preset;
+        same_length = 10 * preset;
+        effort = 100 * preset;
+        skip_length = 1000 * preset;
+    }
 
     bool verbose = false;
     bool parity_context = true;
+    int references = 100000;
     int iterations;
     int length_margin;
     int same_length;
     int effort;
     int skip_length;
-    int references;
 };
 
 class shrinkler_compressor
