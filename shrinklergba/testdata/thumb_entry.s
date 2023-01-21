f616d78aaae256c514a42c45261be82146502184
tom42 <tom42@users.noreply.github.com>
2023-01-21 08:32:47 +0100

    Fixed/added copyright messages of remaining files written by me.

--- shrinklergba/testdata/thumb_entry.s
+++ shrinklergba/testdata/thumb_entry.s
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2021 Thomas Mathys
+# SPDX-License-Identifier: MIT
+# shrinkler-gba: Port of the Shrinkler Amiga executable cruncher for the GBA
+
                 .cpu    arm7tdmi
                 .text
                 .global _start
