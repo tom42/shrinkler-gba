897f551c427935636b22e84b2efb13d363867e75
tom42 <tom42@users.noreply.github.com>
2025-08-30 23:44:27 +0200

    Move test utilities header

---
+++ test/shrinkler_gba_unit_test/test_utilities.hpp
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2021 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#ifndef SHRINKLER_GBA_UNIT_TEST_TEST_UTILITIES_HPP
+#define SHRINKLER_GBA_UNIT_TEST_TEST_UTILITIES_HPP
+
+#include <filesystem>
+#include <vector>
+
+namespace shrinkler_gba_unit_test
+{
+
+// TODO: do we need <filesystem> here?
+// TODO: consider turning this into a module?
+std::vector<unsigned char> load_binary_file(const std::filesystem::path& filename);
+const std::vector<unsigned char> make_bytevector(const char* s);
+
+}
+
+#endif
