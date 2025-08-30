7fd561ffd9585d5e076b88016230d0b44a5b0e2a
tom42 <tom42@users.noreply.github.com>
2025-08-30 12:29:25 +0200

    Get adler32 implementation

--- src/shrinkler_gba/adler32.cpp
+++ src/shrinkler_gba/adler32.cpp
@@ -1,23 +1,23 @@
 // SPDX-FileCopyrightText: 2023 Thomas Mathys
 // SPDX-License-Identifier: MIT
-// shrinkler-gba: Port of the Shrinkler Amiga executable cruncher for the GBA
 
-#include "shrinklergbacore/adler32.hpp"
+module shrinkler_gba;
 
-namespace shrinklergbacore
+namespace shrinkler_gba
 {
 
+// TODO: are we not missing headers here, like <cstdint> and <vector>? That's just MSVC not needing them, no?
 uint32_t adler32(const std::vector<unsigned char>& data)
 {
-    constexpr auto BASE = 65521;
+    constexpr auto base = 65521;
 
     uint32_t s1 = 1;
     uint32_t s2 = 0;
 
     for (auto byte : data)
     {
-        s1 = (s1 + byte) % BASE;
-        s2 = (s2 + s1) % BASE;
+        s1 = (s1 + byte) % base;
+        s2 = (s2 + s1) % base;
     }
 
     return (s2 << 16) | s1;
