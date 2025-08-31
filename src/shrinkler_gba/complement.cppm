8f657f5ab3891ce99b6ac7e13e54ee371d6d61f8
tom42 <tom42@users.noreply.github.com>
2025-08-31 23:04:58 +0200

    Convert complement_test to catch2
    Does not link, apparently we're still missing complement.cpp.

--- src/shrinkler_gba/complement.cppm
+++ src/shrinkler_gba/complement.cppm
@@ -12,11 +12,13 @@ namespace shrinkler_gba
 {
 
 // The size of the checksummed area in the GBA cartridge header in bytes, including the complement byte.
+SHRINKLER_GBA_EXPORT_FOR_UNIT_TESTING
 inline constexpr size_t checksum_area_size = ofs_complement - ofs_game_title + 1;
 
 // Calculates the complement of a GBA cartridge header.
 // checksum_area must point to a buffer representing the cartridge's checksum area.
 // This buffer must be at least checksum_area_size bytes big.
+SHRINKLER_GBA_EXPORT_FOR_UNIT_TESTING
 unsigned char calculate_complement(const unsigned char* checksum_area);
 
 // Calculates the complement of a GBA cartridge header, but allows to choose which of the bytes
@@ -25,6 +27,7 @@ unsigned char calculate_complement(const unsigned char* checksum_area);
 // Obviously this has no practical purpose, except for extreme size optimization.
 // checksum_area must point to a buffer representing the cartridge's checksum area.
 // This buffer must be at least checksum_area_size bytes big.
+SHRINKLER_GBA_EXPORT_FOR_UNIT_TESTING
 unsigned char calculate_complement(const unsigned char* checksum_area, size_t variable_byte_index);
 
 }
---
+++ test/shrinkler_gba_unit_test/complement_test.cpp
@@ -0,0 +1,68 @@
+// SPDX-FileCopyrightText: 2023 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import shrinkler_gba;
+
+namespace shrinkler_gba_unit_test
+{
+
+using shrinkler_gba::calculate_complement;
+
+namespace
+{
+
+const unsigned char all_zero_except_fixed_byte[shrinkler_gba::checksum_area_size] =
+{
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x96, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+const unsigned char ascending_numbers[shrinkler_gba::checksum_area_size] =
+{
+    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e
+};
+
+}
+
+TEST_CASE("complement_test")
+{
+    SECTION("value of checksum_area_size is correct")
+    {
+        CHECK(shrinkler_gba::checksum_area_size == 30);
+    }
+
+    SECTION("calculate complement normally")
+    {
+        CHECK(calculate_complement(all_zero_except_fixed_byte) == 0x51);
+        CHECK(calculate_complement(ascending_numbers) == 0x34);
+    }
+}
+
+}
+
+// TODO: convert stuff below
+//
+//    BOOST_AUTO_TEST_CASE(calculate_complement_with_fixed_complement_byte)
+//    {
+//        std::vector<unsigned char> buf(ascending_numbers, ascending_numbers + std::size(ascending_numbers));
+//
+//        // We want the real complement byte to have a fixed value, and fix the game version byte instead.
+//        // Calculate what value the game version byte should have.
+//        const size_t game_version_index = ofs_game_version - ofs_game_title;
+//        const unsigned char game_version_byte = calculate_complement(buf.data(), game_version_index);
+//        BOOST_TEST(game_version_byte == 0x33);
+//
+//        // Patch the game version byte in the checksum area, and check whether everything matches up.
+//        buf[game_version_index] = game_version_byte;
+//        const unsigned char calculated_complement_byte = calculate_complement(buf.data());
+//        BOOST_TEST(calculated_complement_byte == 0x1e);
+//        BOOST_TEST(buf[checksum_area_size - 2] == 0x33); // Game version
+//        BOOST_TEST(buf[checksum_area_size - 1] == 0x1e); // Complement byte which we forced to a desired value
+//    }
+//
+//BOOST_AUTO_TEST_SUITE_END()
+//
+//}
