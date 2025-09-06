71c11c92b5daed647b7826dd11697454a298e59c
tom42 <tom42@users.noreply.github.com>
2025-09-06 09:32:23 +0200

    Remove todo item: it is not possible to overload by return type.

--- src/shrinkler_gba/command_line.cpp
+++ src/shrinkler_gba/command_line.cpp
@@ -59,7 +59,7 @@ parse_command_line_result parse_command_line(int argc, char* argv[], argpppp::pf
     parser.set_nargs(1);
 
     add_header(parser, "General options:");
-    add_option(parser, { "output-file", 'o', "FILE", {}, "Specify output filename. The default output filename is the input filename with the extension replaced by .gba" }, [&](auto s) { result.options.output_file(s); return true; }); // TODO: can we shorten this? Is it possible to overload stuff such that the return can be omitted if we do not wish to check anything?
+    add_option(parser, { "output-file", 'o', "FILE", {}, "Specify output filename. The default output filename is the input filename with the extension replaced by .gba" }, [&](auto s) { result.options.output_file(s); return true; });
     add_option(parser, { "verbose", 'v', {}, {}, "Print verbose messages", 0 }, [&](auto) { result.options.verbose(true); return true; });
 
     add_header(parser, "Depacker options:");
