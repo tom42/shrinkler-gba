0960d0d5b7fb0da5d45c2d941659bd827904c5a3
tom42 <tom42@users.noreply.github.com>
2025-11-22 17:11:55 +0100

    Set up input_file_test

---
+++ test/shrinkler_gba_unit_test/input_file_test.cpp
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2021 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import shrinkler_gba;
+
+namespace shrinkler_gba_unit_test
+{
+
+TEST_CASE("input_file_test")
+{
+}
+
+}
