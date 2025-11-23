7818aae2d081d1c991cd722fe538070a3bfac06c
tom42 <tom42@users.noreply.github.com>
2025-11-23 18:38:14 +0100

    Turn class gba_packer into a function

--- src/shrinkler_gba/gba_packer.cpp
+++ src/shrinkler_gba/gba_packer.cpp
@@ -6,16 +6,39 @@ module shrinkler_gba;
 namespace shrinkler_gba
 {
 
-void gba_packer::pack(const options&)
+namespace
 {
+
+console create_console(const options& opts)
+{
+    console c;
+
+    if (!opts.verbose())
+    {
+        c.verbose_stream(nullptr);
+    }
+
+    return c;
+}
+
+}
+
+void pack(const options& opts)
+{
+    auto console = create_console(opts);
+
     // TODO: implement the existing 5/6/whatever steps:
-    //       * Set up the console/logging/whatever
     //       * Load program (load ELF and convert to raw binary)
     //       * Compress raw binary: note: here some work on libshrinkler might be necessary: I am not sure it supports progress output. But that's fine.
     //       * Assemble cart
     //       * Fix up for EZFlash (why is that not done by the assembler? => because that's post processing and needs to be done for both assemblers?)
     //       * Write result to disk
     // TODO: here we would like to rework the existing components, so that they do NOT produce console output. All console output should be controlled/done up here in this file
+}
+
+// TODO: old stuff below (packer-as-a-class: probably delete this once we do not need it anymore)
+void gba_packer::pack(const options&)
+{
     initialize_console_output();
     load_input_file();
 }
