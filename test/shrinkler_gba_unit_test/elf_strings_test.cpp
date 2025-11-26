cf97c12af51a4361648b880ba7c56086c77b8f68
tom42 <tom42@users.noreply.github.com>
2025-11-26 17:11:21 +0100

    Set up test case for elf_strings

---
+++ test/shrinkler_gba_unit_test/elf_strings_test.cpp
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2025 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import shrinkler_gba;
+
+namespace shrinkler_gba_unit_test
+{
+
+TEST_CASE("elf_strings")
+{
+	SECTION("to_hex")
+	{
+		// TODO: check to_hex without width spec
+	}
+}
+
+}
