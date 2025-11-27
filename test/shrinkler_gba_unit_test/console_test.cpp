7a5ef8b786341c26dfc4337fda78c114c92f4597
tom42 <tom42@users.noreply.github.com>
2025-11-27 10:45:17 +0100

    Set up test for console

---
+++ test/shrinkler_gba_unit_test/console_test.cpp
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025 Thomas Mathys
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+import shrinkler_gba;
+
+namespace shrinkler_gba_unit_test
+{
+}
