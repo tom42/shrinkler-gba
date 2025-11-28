94e9edb1be3c27dd4727f757b46305dbc356100f
tom42 <tom42@users.noreply.github.com>
2025-11-28 10:33:16 +0100

    Console: improve tests: actually log formatted output

--- test/shrinkler_gba_unit_test/console_test.cpp
+++ test/shrinkler_gba_unit_test/console_test.cpp
@@ -20,64 +20,64 @@ TEST_CASE("console")
     console.warn_stream(&warn_stream);
     console.verbose_stream(&verbose_stream);
 
-    SECTION("out, output is disabled")
+    SECTION("out, formatted, output is disabled")
     {
         console.out_stream(nullptr);
 
-        console.out("log message");
+        console.out("log message: {}", 42);
 
         CHECK(out_stream.str() == "");
         CHECK(warn_stream.str() == "");
         CHECK(verbose_stream.str() == "");
     }
 
-    SECTION("out, output is enabled")
+    SECTION("out, formatted, output is enabled")
     {
-        console.out("log message");
+        console.out("log message: {}", 42);
 
-        CHECK(out_stream.str() == "log message\n");
+        CHECK(out_stream.str() == "log message: 42\n");
         CHECK(warn_stream.str() == "");
         CHECK(verbose_stream.str() == "");
     }
 
-    SECTION("warn, output is disabled")
+    SECTION("warn, formatted, output is disabled")
     {
         console.warn_stream(nullptr);
 
-        console.warn("log message");
+        console.warn("log message: {}", 42);
 
         CHECK(out_stream.str() == "");
         CHECK(warn_stream.str() == "");
         CHECK(verbose_stream.str() == "");
     }
 
-    SECTION("warn, output is enabled")
+    SECTION("warn, formatted output is enabled")
     {
-        console.warn("log message");
+        console.warn("log message: {}", 42);
 
         CHECK(out_stream.str() == "");
-        CHECK(warn_stream.str() == "Warning: log message\n");
+        CHECK(warn_stream.str() == "Warning: log message: 42\n");
         CHECK(verbose_stream.str() == "");
     }
 
-    SECTION("verbose, output is disabled")
+    SECTION("verbose, formatted, output is disabled")
     {
         console.verbose_stream(nullptr);
 
-        console.verbose("log message");
+        console.verbose("log message: {}", 42);
 
         CHECK(out_stream.str() == "");
         CHECK(warn_stream.str() == "");
         CHECK(verbose_stream.str() == "");
     }
 
-    SECTION("verbose, output is enabled")
+    SECTION("verbose, formatted, output is enabled")
     {
-        console.verbose("log message");
+        console.verbose("log message: {}", 42);
 
         CHECK(out_stream.str() == "");
         CHECK(warn_stream.str() == "");
-        CHECK(verbose_stream.str() == "log message\n");
+        CHECK(verbose_stream.str() == "log message: 42\n");
     }
 
     SECTION("is_verbose_enabled")
