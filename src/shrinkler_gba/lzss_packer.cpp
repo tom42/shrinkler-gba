c821c731ad9da159ef746632c39d26b257ca4a42
tom42 <tom42@users.noreply.github.com>
2025-12-09 10:38:53 +0100

    Implement lzss compression

--- src/shrinkler_gba/lzss_packer.cpp
+++ src/shrinkler_gba/lzss_packer.cpp
@@ -1,7 +1,13 @@
 // SPDX-FileCopyrightText: 2025 Thomas Mathys
 // SPDX-License-Identifier: MIT
 
+module;
+
+#include <iterator>
+#include <vector>
+
 module shrinkler_gba;
+import agbpack;
 
 namespace shrinkler_gba
 {
@@ -9,21 +15,24 @@ namespace shrinkler_gba
 namespace
 {
 
-std::vector<unsigned char> lzss_compress(const std::vector<unsigned char>& /*data*/)
+std::vector<unsigned char> lzss_compress(const std::vector<unsigned char>& input)
 {
-	// TODO: compress and return data
-	return {};
+	std::vector<unsigned char> output;
+	agbpack::optimal_lzss_encoder lzss_encoder;
+	lzss_encoder.encode(input.begin(), input.end(), back_inserter(output));
+	return output;
 }
 
 }
 
-cartridge lzss_packer::pack(const input_file& /*input_file*/)
+cartridge lzss_packer::pack(const input_file& input_file)
 {
 	// TODO: real implementation
 	//       * pack
 	//         * lzss (move from pack.cpp)
 	//         * huffman (move from pack.cpp)
 	//       * assemble
+	auto x = lzss_compress(input_file.data()); // TODO: name: x
 	return {};
 }
 
