fba0577cac9155740f1bcd3a0654964c07b89126
tom42 <tom42@users.noreply.github.com>
2026-01-12 19:16:10 +0100

    Paste in code from cmake wiki, but that does not work yet

--- cmake/VtgAddUninstallTarget.cmake
+++ cmake/VtgAddUninstallTarget.cmake
@@ -18,7 +18,20 @@
 #     include(VtgAddUninstallTarget)
 #   endif()
 
+# Add uninstall target
+if(NOT TARGET uninstall)
+  configure_file(
+    "${CMAKE_CURRENT_SOURCE_DIR}/vtg_cmake_uninstall.cmake.in"
+    "${CMAKE_CURRENT_BINARY_DIR}/vtg_cmake_uninstall.cmake"
+    IMMEDIATE @ONLY)
+
+  add_custom_target(
+    uninstall
+    COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake)
+endif()
+
 # TODO:
+# * Fix the code above, so that the in-file above is found
 # * Add documentation:
 #   * how to use: include in top level CMakeLists.txt, but preferably only if we're the top level project (we should not add unsolicited uninstall targets)
 #   * Where to place this file: cmake directory (and along with it the other cmake script we're going to configure)
