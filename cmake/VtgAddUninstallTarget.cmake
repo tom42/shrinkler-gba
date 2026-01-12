8a92056f47b3ad8eaef8951dadcbe1b25bdb9b52
tom42 <tom42@users.noreply.github.com>
2026-01-12 08:23:40 +0100

    Sketch out make uninstall target

---
+++ cmake/VtgAddUninstallTarget.cmake
@@ -0,0 +1,10 @@
+# TODO:
+# * Add documentation:
+#   * how to use: include in top level CMakeLists.txt, but preferably only if we're the top level project (we should not add unsolicited uninstall targets)
+#   * Where to place this file: cmake directory (and along with it the other cmake script we're going to configure)
+#   * Where did it come from (CMake wiki)
+# * License: since this is nothing substantial, use the license from the CMake Wiki
+#   * https://gitlab.kitware.com/cmake/community/-/wikis/FAQ#what-is-its-license
+#     "CMake is distributed under the OSI-approved BSD 3-clause License. See the licensing page for details. The snippets on this wiki are provided under the same license."
+# * Implement it
+#   * Well actually this is going to get the 2nd portion of that snippet from the Wiki, no?
