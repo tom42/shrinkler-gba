449ba3e6f0087652d6cea6e9945e6406fe619f6d
tom42 <tom42@users.noreply.github.com>
2026-01-25 15:44:15 +0100

    Packer registry: simplify: use constinit array, not vector.

--- src/shrinkler_gba/packer.cpp
+++ src/shrinkler_gba/packer.cpp
@@ -1,10 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Thomas Mathys
 // SPDX-License-Identifier: MIT
 
-module;
-
-#include <vector>
-
 module shrinkler_gba;
 
 namespace shrinkler_gba
@@ -13,21 +9,11 @@ namespace shrinkler_gba
 namespace
 {
 
-constinit const std::vector<packer_info> packers
+constinit const packer_info packers[]
 {
     { "lzss" },
     { "shrinkler" }
 };
-
-}
-
-namespace packer_registry
-{
-
-const std::vector<packer_info>& all_packers()
-{
-    return packers;
-}
 
 }
 
